b3de99dc60899709327e80cb33efa45f890ba9d7
ChipMorris10 <goldcomac@mac.com>
2015-09-06 17:53:43 -0600

    commiting old exercises

--- main.js
+++ main.js
@@ -1,21 +1,26 @@
 // Exercise 1
+// send an alert when milk is clicked
 var milk = document.getElementsByTagName('li')[0];
 
 milk.addEventListener("click", function(event) {
     alert("You clicked milk");
     milk.addEventListener("click", function(event) {
-        alert("You really shouldn't drink milk");
+        alert("Yum");
     });
 });
 
+
 // Exercise 2
+// turn the word "wine" red when clicked
 var wine = document.getElementsByTagName('li')[3];
 
 wine.addEventListener("click", function(event) {
     this.style.color = "red";
 });
 
+
 // Exercise 3
+// hide the reset button when the mouse rolls over it
 var reset = document.getElementById("reset");
 
 reset.addEventListener("mouseenter", function(event) {
@@ -24,8 +29,9 @@ reset.addEventListener("mouseenter", function(event) {
 
 
 // Exercise 4
+// change the picture to beer and add 10 of them to the DOM when clicked
 var honeyPicture = addEventListener("dblclick", function() {
- var honeyPicture = document.getElementById('honey');
+var honeyPicture = document.getElementById('honey');
 
     for(var i = 0; i <= 10; i++) {
         var clonedImage = honeyPicture.cloneNode(true);
@@ -33,28 +39,54 @@ var honeyPicture = addEventListener("dblclick", function() {
     }
 });
 
+myNode = document.querySelector('#honey');
+
+myNode.addEventListener("dblclick", function() {
+    myNode.src = "./images/beer.jpeg";
+});
+
+
 // Exercise 5
-// append the beer picture to the DOM
+// add the panic.jpeg picture to the DOM on load
+var myElement = document.createElement('img');
+
+myElement.src = "./images/panic.jpeg";
+
+var addPicture = document.querySelectorAll('#essentials ul li');
+addPicture[0].appendChild(myElement);
 
 
 // Exercise 6
-var honey = document.getElementsByTagName("li")[1];
-
-honey.addEventListener("click", function(event) {
-    this.element.innerHTML = "buzz buzz";
-});
-
-// Exercise 7
+// turn the word "beer" brown when clicked
 var beer = document.getElementsByTagName("li")[4];
 
 beer.addEventListener("click", function() {
     this.style.color = "brown";
 });
 
-// Exercise 8
+// Exercise 7
+// increase the word "water" and turn it red when the mouse enters the area
 var water = document.getElementsByTagName("li")[2];
 
 water.addEventListener("mouseenter", function() {
     this.style.fontSize = "35px";
     this.style.color = "red";
 });
+
+//  Exercise 8
+// doesn't do anything
+// newNode = document.querySelector('#essentials li')[1];
+
+// newNode.addEventListener("click", function () {
+//     newNode.innerHTML = "coffee";
+// });
+
+// Exercise 9
+// adds a paragraph to the DOM
+var pNode = document.createElement('p');
+var myText = document.createTextNode("I added this paragraph instructing you what to do.");
+
+newNode = document.querySelector('.content');
+
+pNode.appendChild(myText);
+newNode.insertBefore(pNode, newNode.childNodes[2]);
